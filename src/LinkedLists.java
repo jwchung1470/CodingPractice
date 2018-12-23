e7ac7e66907a6412cdb3ac0bcddd972261afe060
jwchung1470 <BufordStudent@gmail.com>
2018-12-23 13:18:48 -0500

    linked list

---
+++ src/LinkedLists.java
@@ -0,0 +1,12 @@
+/*
+======================================================================================
+Author: Jin W. Chung
+This is my practice for technical interview questions
+from Cracking the Coding Interview by Gayle Laakmann Mcdowell
+======================================================================================
+ */
+
+
+
+public class LinkedLists {
+}
